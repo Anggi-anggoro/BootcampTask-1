87d7acabc77b8f0d612cb6ca87e057cf40c0be1e
Anggi-anggoro <anggi.anggoro26@gmail.com>
2022-11-14 16:31:24 +0700

    Init commit

---
+++ bin/custom/training/trainingtest/gensrc/com/hybris/training/test/constants/GeneratedTrainingTestConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 14 Nov 2022 11.21.41                        ---
+ * ----------------------------------------------------------------
+ */
+package com.hybris.training.test.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingTestConstants
+{
+	public static final String EXTENSIONNAME = "trainingtest";
+	
+	protected GeneratedTrainingTestConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
